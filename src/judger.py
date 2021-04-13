712949baa04b74b529a2199d03e6eea4758e66c6
Jijie LIU <liujijieseason@gmail.com>
2021-04-13 18:53:58 +0200

    Update judger.py

--- src/judger.py
+++ src/judger.py
@@ -1,5 +1,5 @@
 from typing import Any
-
+import math
 import numpy as np
 
 
@@ -27,14 +27,18 @@ class Judger:
        identify if the mask is a signature
     """
 
-    def __init__(self, size_ratio=[1, 4], pixel_ratio=[0.01, 1]) -> None:
+    def __init__(
+        self, size_ratio=[1, 4], pixel_ratio=[0.01, 1], border_ratio=0.1
+    ) -> None:
         self.size_ratio = size_ratio
         self.pixel_ratio = pixel_ratio
+        self.border_ratio = border_ratio
 
     def __str__(self) -> str:
         s = "\nJudger\n==========\n"
         s += "size_ratio = {}\n".format(str(self.size_ratio))
         s += "pixel_ratio = {}\n".format(str(self.pixel_ratio))
+        s += "border_ratio = {}\n".format(str(self.border_ratio))
         return s
 
     def _is_valid_mask(self, mask: Any) -> bool:
@@ -45,8 +49,16 @@ class Judger:
             raise Exception("The input np.array should only have 2 value, 0 and 255.")
         return True
 
+    def _remove_borders(self, mask: Any) -> Any:
+        """
+        remove the borders around the mask
+        """
+        border = math.floor(min(mask.shape) * self.border_ratio)
+        return mask[border : mask.shape[0] - border, border : mask.shape[1] - border]
+
     def judge(self, mask: Any) -> bool:
         if self._is_valid_mask(mask):
+            mask = self._remove_borders(mask)
             size_ratio = max(mask.shape) / min(mask.shape)
             if size_ratio < self.size_ratio[0] or size_ratio > self.size_ratio[1]:
                 return False
