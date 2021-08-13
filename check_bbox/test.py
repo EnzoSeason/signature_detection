38d785bece34d63835031ea649c91deb42590ac4
Purefekt <roonyyo007@gmail.com>
2021-08-13 22:52:08 +0200

    added module for bbox

--- check_bbox/test.py
+++ check_bbox/test.py
@@ -2,12 +2,8 @@ from signature_detect.loader import Loader
 from signature_detect.cropper import Cropper
 from signature_detect.extractor import Extractor
 from signature_detect.bounding_box import Bounding_box
-import cv2
 
-path = '/Users/veersingh/Desktop/docs_with_signs_dataset(tobacco800)/images/aam09c00.tif'
-
-
-image = cv2.imread(path)
+path = 'check_bbox/aam09c00.tif'
 
 # Loader
 loader = Loader()
@@ -21,7 +17,7 @@ labeled_mask = extractor.extract(mask)
 cropper = Cropper()
 results = cropper.run(labeled_mask)
 
-# bbox
+# Bbox
 check = Bounding_box().run(labeled_mask)
 print(check)
 # check value = [1024, 1443, 546, 96]
